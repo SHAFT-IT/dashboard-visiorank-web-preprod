ae97268d64704add8e6c4f05a80886b65ba3df7f
randrianarivo-mandimby <mandimby.randrianarivo@shaft-it.com>
2018-07-27 14:47:35 +0200

    mimtype of file return

--- app/Http/Controllers/Api/V1/DemandesController.php
+++ app/Http/Controllers/Api/V1/DemandesController.php
@@ -257,8 +257,9 @@ class DemandesController extends Controller
         foreach ($toPjTicket as $oPjTicket) {
             $oPjTicket->icon = 'fa-file-o';
             if (isset($oPjTicket->pj_file) && $oPjTicket->pj_file != "") {
-                $oPjTicket->icon = $this->getIconByFileMimeType(mime_content_type(public_path() . "/uploads/" . $oPjTicket->pj_file));
+                $oPjTicket->pj_icon = $this->getIconByFileMimeType(mime_content_type(public_path() . "/uploads/" . $oPjTicket->pj_file));
                 $oPjTicket->pj_file = Request::getHttpHost() . "/public/uploads/" . $oPjTicket->pj_file;
+                $oPjTicket->pj_type = mime_content_type(public_path() . "/uploads/" . $oPjTicket->pj_file);
             }
         }
         return $toPjTicket;
